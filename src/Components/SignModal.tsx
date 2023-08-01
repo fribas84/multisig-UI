9101640210e90fc13118c56a1719cb953fa092a4
Fernando Ribas <fribas84@gmail.com>
2023-07-31 22:31:03 -0300

    modal sign desing

---
+++ src/Components/SignModal.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import { Button, Modal } from 'flowbite-react';
+import {FiAlertCircle} from 'react-icons/fi';
+import { formatEther } from 'viem';
+
+interface Props  {
+    id:number
+    withdraw: IWithdraw;
+    showSignModal: boolean;
+    setShowSignModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const SignModal = ({id,withdraw,showSignModal,setShowSignModal}: Props) => {
+  
+    return (
+    <Modal show={showSignModal} size="lg" popup onClose={()=>setShowSignModal(false)}>
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <FiAlertCircle className="mx-auto mb-4 h-14 w-14 text-yellow-200 dark:text-yellow-100" />
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-100">
+              Do you want to Sign Withdraw ID {id}?
+            </h3>
+            <div className='text-black dark:text-white text-left m-auto'>
+            <p>Amount: {formatEther(withdraw.amount).toString()} eth</p>
+            <p>Destination: {withdraw.to}</p>
+            <p>Signers: {withdraw.approvals.toString()}</p>
+            </div>
+            <div className="flex justify-center gap-4 mt-4">
+              <Button color="failure">
+                Yes, I'm sure
+              </Button>
+              <Button color="gray">
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    
+  )
+}
+
+export default SignModal
--- src/Components/WithdrawTable.tsx
+++ src/Components/WithdrawTable.tsx
@@ -1,41 +1,52 @@
 import { Badge, Button, Table } from 'flowbite-react'
+import { useState } from 'react';
 import { formatEther } from 'viem';
+import SignModal from './SignModal';
 
-interface Props  {
+interface Props {
     index: number;
-    withdraw : IWithdraw;
+    withdraw: IWithdraw;
 }
 
-const WithdrawTable = ({index, withdraw}: Props) => {
-   
+const WithdrawTable = ({ index, withdraw }: Props) => {
+    const [showSignModal, setShowSignModal] = useState<boolean>(false);
     return (
-        
-        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800" key={index}>
-                      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                        {index}
-                      </Table.Cell>
-                      <Table.Cell>
-                        {withdraw?.to}
-                      </Table.Cell>
-                      <Table.Cell>
-                        {formatEther(withdraw.amount).toString()} eth
-                      </Table.Cell>
-                      <Table.Cell>
-                        {withdraw.approvals.toString()}
-                      </Table.Cell>
 
-                      <Table.Cell>
-                        <Badge color={withdraw.sent === true ? "success" : "warning"}>
-                          {withdraw?.sent.toString()}
-                        </Badge>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <Button className="hover:scale-110" disabled={withdraw?.sent}>
-                          Sign
-                        </Button>
-                      </Table.Cell>
-                    </Table.Row>
-  )
+        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800" key={index}>
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {index}
+            </Table.Cell>
+            <Table.Cell>
+                {withdraw?.to}
+            </Table.Cell>
+            <Table.Cell>
+                {formatEther(withdraw.amount).toString()} eth
+            </Table.Cell>
+            <Table.Cell>
+                {withdraw.approvals.toString()}
+            </Table.Cell>
+
+            <Table.Cell>
+                <Badge color={withdraw.sent === true ? "success" : "warning"}>
+                    {withdraw?.sent.toString()}
+                </Badge>
+            </Table.Cell>
+            <Table.Cell>
+                <Button
+                className="hover:scale-110"
+                disabled={withdraw?.sent}
+                onClick={()=>setShowSignModal(true)}>
+                    Sign
+                </Button>
+            </Table.Cell>
+            <SignModal
+                id={index}
+                withdraw={withdraw}
+                showSignModal={showSignModal}
+                setShowSignModal={setShowSignModal}
+            />
+        </Table.Row>
+    )
 }
 
 export default WithdrawTable
